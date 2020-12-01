755e39da40f889acf3571f0770e4e2a0fcec79b9
tom reifenberg <reifenberg.57@gmail.com>
2020-12-01 11:33:51 -0600

    Using Custom Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
