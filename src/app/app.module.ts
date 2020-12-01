abd333d3161d641806de1012d8eef34e913e8b22
tom reifenberg <reifenberg.57@gmail.com>
2020-12-01 15:57:18 -0600

    Creating and Nesting Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
 // import { HttpModule } from '@angular/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
